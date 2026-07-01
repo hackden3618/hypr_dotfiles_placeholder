a842040931e188ad0a42c7d18c160b26fc8fed4c
hackden3618 <denniswambugu@protonmail.com>
2026-07-01 20:45:49 +0300

    preference in the opacity, to have a little translucence, coz why not

--- hyprlua/UserConfigs/UserDecorations.lua
+++ hyprlua/UserConfigs/UserDecorations.lua
@@ -22,8 +22,8 @@ hl.config({
     },
     decoration = {
         rounding = 10,
-        active_opacity = 1.0,
-        inactive_opacity = 0.9,
+        active_opacity = 0.95,
+        inactive_opacity = 0.85,
         fullscreen_opacity = 1.0,
         dim_inactive = true,
         dim_strength = 0.1,
